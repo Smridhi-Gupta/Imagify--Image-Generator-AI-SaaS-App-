ca2971e78043d23a0f03afb8bab5a89fcf161438
Smridhi gupta <smridhi106@gmail.com>
2024-12-29 18:17:37 +0530

    init

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Description = () => {
   return (
-    <div>
-        <h1></h1>
+    <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+        <h1 className='text-3xl sm:text-4xl font-semibold mb-2'> Create AI Images</h1>
+        <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
     </div>
   )
 }
