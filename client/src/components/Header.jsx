700de34bf9bdd62e300d52451a80f6973360b260
Smridhi gupta <smridhi106@gmail.com>
2024-12-29 15:35:16 +0530

    init

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center text-center my-20">
+      <div className="text-stone-500 inline-flex text-center gap-2 bg-white px-6">
+        <p>Best text to image generator</p>
+        <img src={assets.star_icon} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
