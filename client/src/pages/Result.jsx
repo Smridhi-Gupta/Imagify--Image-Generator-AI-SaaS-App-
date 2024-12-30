3218dc8e4596ab807517a774832b16ef1ae1e5cf
Smridhi gupta <smridhi106@gmail.com>
2024-12-30 15:18:54 +0530

    init

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -4,16 +4,21 @@ import { useState } from "react";
 
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
-  const [isImageLoaded, setIsImageLoaded] = useState(true);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
     <form className="flex flex-col min-h-[90vh] justify-center items-center">
       <div>
         <div className="relative">
           <img src={image} alt="" className="max-w-sm rounded"></img>
-          <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"></span>
+          <span
+            className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${
+              loading ? "w-full transition-all duration-[10s]" : "w-0"
+            }`}
+          ></span>
         </div>
-        <p>Loading.....</p>
+        <p className={!loading ? "hidden" : ""}>Loading.....</p>
       </div>
       {!isImageLoaded && (
         <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
@@ -30,19 +35,25 @@ const Result = () => {
           </button>
         </div>
       )}
-
-      <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
-        <p className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">
-          Generate Another
-        </p>
-        <a
-          href={image}
-          download
-          className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
-        >
-          Download
-        </a>
-      </div>
+      {isImageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+          <p
+            onclick={() => {
+              setIsImageLoaded(false);
+            }}
+            className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer"
+          >
+            Generate Another
+          </p>
+          <a
+            href={image}
+            download
+            className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
+          >
+            Download
+          </a>
+        </div>
+      )}
     </form>
   );
 };
