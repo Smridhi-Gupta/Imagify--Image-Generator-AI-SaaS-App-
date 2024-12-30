1eb466d61da33150fb44f3d7427dfea3d0ca240e
Smridhi gupta <smridhi106@gmail.com>
2024-12-30 19:14:21 +0530

    init

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Login = () => {
   return (
-    <div className='absolute top-0 let-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
+    <div className="absolute top-0 let-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          Sign Up
+        </h1>
+        <p className="text-sm">Welcome back! Please sign in to continue</p>
+        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
+          <img src={assets.user_icon} alt=""></img>
+          <input
+            type="text"
+            className="outline-none text-sm"
+            placeholder="Full Name"
+            required
+          ></input>
+        </div>
 
+        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
+          <img src={assets.email_icon} alt=""></img>
+          <input
+            type="email"
+            className="outline-none text-sm"
+            placeholder="Email id"
+            required
+          ></input>
+        </div>
+
+        <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-4">
+          <img src={assets.lock_icon} alt=""></img>
+          <input
+            type="password"
+            className="outline-none text-sm"
+            placeholder="Password"
+            required
+          ></input>
+        </div>
+        <p className="text-sm text-blue-600 my-4 cursor-pointer">
+          Forgot Password
+        </p>
+
+        <button className="bg-blue-600 w-full text-white py-2 rounded-full">
+          Create Account
+        </button>
+
+        <p className="mt-5 text-center">
+          Don't have an account?
+          <span className="text-blue-600 cursor-pointer">Sign up</span>
+        </p>
+
+        <p className="mt-5 text-center">
+          Don't have an account?
+          <span className="text-blue-600 cursor-pointer">Sign up</span>
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
