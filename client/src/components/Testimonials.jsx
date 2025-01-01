5178bee0bb97240155cc7752d326101b8f496434
Smridhi gupta <smridhi106@gmail.com>
2025-01-01 20:48:24 +0530

    init

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { assets, testimonialsData } from "../assets/assets";
+import { motion } from "framer-motion";
 
 const Testimonials = () => {
   return (
-    <div className="flex flex-col items-center justify-center my-20 py-12">
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      className="flex flex-col items-center justify-center my-20 py-12"
+    >
       <h1 className="text-3xl sm:text-4xl font-semibold mb-2">
         Customer Testimonials
       </h1>
@@ -12,7 +19,10 @@ const Testimonials = () => {
       {/* assets.jsx se data */}
       <div className="flex flex-wrap gap-6">
         {testimonialsData.map((testimonial, index) => (
-          <div key={index} className="bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all">
+          <div
+            key={index}
+            className="bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all"
+          >
             <div className="flex flex-col items-center">
               <img
                 src={testimonial.image}
@@ -28,12 +38,14 @@ const Testimonials = () => {
                     <img key={index} src={assets.rating_star} alt=""></img>
                   ))}
               </div>
-              <p className="text-center text-sm text-gray-600">{testimonial.text}</p>
+              <p className="text-center text-sm text-gray-600">
+                {testimonial.text}
+              </p>
             </div>
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
