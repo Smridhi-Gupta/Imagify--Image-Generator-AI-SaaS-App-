e3cab2a60095e85cdff3036eb7780dbb5f500b4e
Smridhi gupta <smridhi106@gmail.com>
2025-01-04 15:08:23 +0530

    init

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -2,15 +2,42 @@ import React, { useContext } from "react";
 import { assets, plans } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const BuyCredit = () => {
-  const { user, backendUrl, loadCreditsData, token, setShowLogin } = useContext(AppContext);
+  const { user, backendUrl, loadCreditsData, token, setShowLogin } =
+    useContext(AppContext);
+
+  const navigate = useNavigate();
+
+  const initPay = async (order) => {};
+
+  const paymentRazorpay = async (planId) => {
+    try {
+      if (!user) {
+        setShowLogin(true);
+      }
+
+      const { data } = await axios.post(
+        backendUrl + "/api/user/pay-razor",
+        { planId },
+        { headers: { token } }
+      );
+      if (data.success) {
+        initPay(data.order);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <motion.div
       initial={{ opacity: 0.2, y: 100 }}
       transition={{ duration: 1 }}
-      whileInView={{ opacity: 1, y: 0 }}  
+      whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
       className="min-h-[80vh] text-center pt-14 mb-10"
     >
@@ -34,7 +61,10 @@ const BuyCredit = () => {
               <span className="text-3xl font-medium">${item.price} </span>/{" "}
               {item.credits} credits
             </p>
-            <button className="w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52">
+            <button
+              onClick={() => paymentRazorpay(item.id)}
+              className="w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52"
+            >
               {user ? "Purchase" : "Get Started"}
             </button>
           </div>
