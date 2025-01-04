52bbae2f33d51bde89934fc1200cb26bf6df2368
Smridhi gupta <smridhi106@gmail.com>
2025-01-04 14:50:06 +0530

    init

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -120,19 +120,32 @@ const paymentRazorpay = async (req, res) => {
     date = Date.now();
 
     const transactionData = {
-      userId, plan, amount, credits, date
-    }
+      userId,
+      plan,
+      amount,
+      credits,
+      date,
+    };
 
-    const newTransaction = await transactionModel.create(transactionData)
+    const newTransaction = await transactionModel.create(transactionData);
+
+    const options = {
+      amount: amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: newTransaction._id,
+    };
 
     await razorpayInstance.orders.create(options, (error, order) => {
-
-    })
-
+      if (error) {
+        console.log(error);
+        return res.json({ success: false, message: error });
+      }
+      res.json({ success: true, order });
+    });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
-export { registerUser, loginUser, userCredits };
+export { registerUser, loginUser, userCredits, paymentRazorpay };
